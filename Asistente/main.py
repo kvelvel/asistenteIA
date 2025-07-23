91074e65efb59bfebf44247f0106d622f59a96fe
Katia Velasquez <katia.velasquez@gmail.com>
2025-07-23 00:05:36 -0400

    feat(conexión IA): Se genera conexión a Gemini, se intalan librerías necesarias

---
+++ Asistente/main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+#Carga de .env file
+load_dotenv()
+
+#paso de API_KEY a GERMINI
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+#Obtener inpout de usuario a preguntar a gemini
+input_text = input("Sobre que quieres saber: \n ")
+
+
+#"Request a GEMINI "
+response = client.models.generate_content(
+    model=os.getenv("MODELO"), contents=input_text
+)
+print(response.text)
